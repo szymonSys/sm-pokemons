360f0bfafebc4bb905a1e572d72d0e00e7e51659
szymosys <szymosys@MacBook-Pro-szymosys.local>
2026-01-09 15:20:55 +0100

    Adjust pokemon details view

--- pokemons/app/[pokemonName].tsx
+++ pokemons/app/[pokemonName].tsx
@@ -1,23 +1,16 @@
-import { Button, ScrollView, Text } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { useCallback, useEffect, useState } from "react";
-import { Image } from "expo-image";
 import {
   getPokemonDetailsById,
   PokemonDetailsResponse,
 } from "@/apis/pokemons-api";
-import { useStore, Keys } from "@/hooks/use-storage";
+import { PokemonDetails } from "@/components/pokemon-details";
 
 export default function DetailsView() {
   const { pokemonName } = useLocalSearchParams<{ pokemonName: string }>();
   const [pokemonDetails, setPokemonDetails] =
     useState<PokemonDetailsResponse | null>(null);
-  const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
-
-  const storage = useStore<string>(Keys.FavoritePokemon);
-
-  const isFavorite = storage.item === pokemonName;
 
   const handleFetchPokemonDetails = useCallback(async () => {
     setIsLoading(true);
@@ -30,27 +23,5 @@ export default function DetailsView() {
     handleFetchPokemonDetails();
   }, [handleFetchPokemonDetails]);
 
-  async function makePokemonFavorite() {
-    await storage.save(pokemonName);
-  }
-
-  async function unlikePokemon() {
-    await storage.remove();
-  }
-
-  return (
-    <ScrollView>
-      {pokemonDetails ? (
-        <Image
-          source={pokemonDetails.sprites.front_default}
-          style={{ width: 200, height: 200 }}
-        />
-      ) : null}
-      <Button
-        disabled={isLoading || storage.loading}
-        title={isFavorite ? "Unlike" : "Make Favourite"}
-        onPress={isFavorite ? unlikePokemon : makePokemonFavorite}
-      />
-    </ScrollView>
-  );
+  return pokemonDetails ? <PokemonDetails details={pokemonDetails} /> : null;
 }
