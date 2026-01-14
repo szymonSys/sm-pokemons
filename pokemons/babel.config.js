68583f63a61331aadf36d7add520277ab1da554c
szymosys <szymosys@MacBook-Pro-szymosys.local>
2026-01-14 09:48:28 +0100

    Add face detector to camera, configure app

--- pokemons/babel.config.js
+++ pokemons/babel.config.js
@@ -2,6 +2,7 @@
 const workletsPluginOptions = {};
 
 module.exports = {
+  presets: ["babel-preset-expo"],
   plugins: [
     ["react-native-worklets/plugin", workletsPluginOptions],
     ["react-native-worklets-core/plugin"],
