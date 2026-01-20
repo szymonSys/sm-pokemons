9b78a23fe3519cb1c0ac64f14b415f9a8872d817
szymosys <szymosys@MacBook-Pro-szymosys.local>
2026-01-20 10:54:38 +0100

    add usage example

--- pokemons/app/(tabs)/favorite.tsx
+++ pokemons/app/(tabs)/favorite.tsx
@@ -3,6 +3,8 @@ import { useStore, Keys } from "@/hooks/use-storage";
 import { Redirect } from "expo-router";
 import { Button, ScrollView, StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import BrightnessModule from "@/specs/NativeBrightnessModule";
+import { useEffect, useState } from "react";
 
 export default function FavoriteView() {
   const {
@@ -14,6 +16,14 @@ export default function FavoriteView() {
   } = useStore<string>(Keys.FavoritePokemon);
 
   useNavigationEvent("focus", getFavorite);
+  const [hasWriteSettingsPermission, setHasWriteSettingsPermission] = useState<boolean|null>(null);
+  useEffect(() => {
+    BrightnessModule.hasWriteSettingsPermission().then((permission) => {
+      setHasWriteSettingsPermission(permission);
+    });
+  },[])
+
+
 
   if (!favoriteInitialized) {
     return <Redirect href="/" />;
@@ -21,6 +31,15 @@ export default function FavoriteView() {
   return (
     <SafeAreaView style={StyleSheet.absoluteFill}>
       <ScrollView style={styles.wrapper}>
+        {!hasWriteSettingsPermission && <Button title="Request Permission" onPress={() => BrightnessModule.requestWriteSettingsPermission().then((permission) => {
+          setHasWriteSettingsPermission(permission);
+        })} />}
+{hasWriteSettingsPermission && <Button title="Set Brightness" onPress={() => BrightnessModule.setBrightness(0.95).then((val) => {
+          console.log("Brightness set ", val);
+          return Promise.resolve();
+        }).then(()=>BrightnessModule.getBrightness()).then((brightness)=>{
+          console.log("Brightness", brightness);
+        })} />}
         <Text style={styles.text}>
           Favorite Pokemon: {favoritePokemon || "None"}
         </Text>
